94e88f494e7a270a36701224685938152a4ecb20
Alex901 <alexander.winberg.036@gmail.com>
2024-02-24 09:39:31 +0100

    added ToggleButton for future usage

---
+++ src/components/Layout/ToggleButton/ToggleButton.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './ToggleButton.css'; // Import CSS for styling
+
+const ToggleButton = () => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const toggleButton = () => {
+    setIsToggled(!isToggled);
+  };
+
+  return (
+    <div className="toggle-container">
+      <button className={`toggle-button ${isToggled ? 'toggled' : ''}`} onClick={toggleButton}>
+        <div className="toggle-ball"></div>
+      </button>
+    </div>
+  );
+};
+
+export default ToggleButton;
--- src/components/Layout/header/Header.jsx
+++ src/components/Layout/header/Header.jsx
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from "react";
-import './Header.css';
+import "./Header.css";
+import ToggleButton from "../ToggleButton/ToggleButton";
 
 const Header = () => {
-    const [time, setTime] = useState(new Date);
+  const [time, setTime] = useState(new Date());
 
-    useEffect(() => {
-        const intervall = setInterval(() => {
-            setTime(new Date());
-        }, 1000); // update time once per second
+  useEffect(() => {
+    const intervall = setInterval(() => {
+      setTime(new Date());
+    }, 1000); // update time once per second
 
-        return () => clearInterval(intervall);
-    },[])
+    return () => clearInterval(intervall);
+  }, []);
 
-    return (
-        <div className="header">
-            <h1> Todoify </h1>
-            <div> {time.getHours()}:{time.getMinutes()}:{time.getSeconds()} </div>
+  return (
+    <div className="header">
+      <h1> Todoify </h1>
+      <div>
+        {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+        <div className="dark-mode-toggle">
+          <ToggleButton />
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header; 
+export default Header;
