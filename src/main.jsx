2dc6346fa4069ab1574776d2fb5d601ce35e14a1
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-02-26 08:08:31 +0100

    changed page <Title>

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { TodoProvider, useTodoContext } from './contexts/todoContexts.jsx'
+import { TodoProvider } from './contexts/todoContexts.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
