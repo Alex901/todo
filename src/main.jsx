517c3cf488f1bd326c1ca503e2a9a38e139af56b
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-02-29 18:22:33 +0100

    imported UserProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { TodoProvider } from './contexts/todoContexts.jsx'
+import { UserProvider } from './contexts/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <UserProvider>
     <TodoProvider>
     <App />
     </TodoProvider>
+    </UserProvider>
   </React.StrictMode>,
 )
