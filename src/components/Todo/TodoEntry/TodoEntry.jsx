7e61d337b21e082c617b5f28a944c8552238a7ab
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-03-14 21:08:07 +0100

    updated doing and done to new look

--- src/components/Todo/TodoEntry/TodoEntry.jsx
+++ src/components/Todo/TodoEntry/TodoEntry.jsx
@@ -185,8 +185,10 @@ const TodoEntry = ({ type, todoData, onEdit }) => {
                         <button className="deleteButton entryButton" onClick={handleDelete}>
                             <i className="material-icons todo-entry-icon">delete</i>
                         </button>
-                        <button className="moreButton entryButton" onClick={handleMoreInfromationClick}>
-                            <i className="material-icons todo-entry-icon"> {isMoreChecked ? "keyboard_arrow_up" : "keyboard_arrow_down"} </i>
+                        <button className={`moreButton entryButton ${steps.length > 0 || description ? 'has-data' : ''}`} onClick={handleMoreInfromationClick}>
+                            <i className="material-icons todo-entry-icon">
+                                {isMoreChecked ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+                            </i>
                         </button>
                     </div>
                 </div>
@@ -246,72 +248,169 @@ const TodoEntry = ({ type, todoData, onEdit }) => {
         const remainingDays = Math.floor((seconds / 3600 / 24));
 
         return (
-            <div className="todo-entry">
-                <div className="done-item">
-                    <div className="time">
-                        <p className="time-stamp"> <strong>Completed:</strong>
-                            {completed.toLocaleDateString()} - {completed.toLocaleTimeString()}
-                        </p>
-                        <div className="separator"> </div>
-                        <p className="time-stamp">
-                            <strong> Duration: </strong>
-                            {remainingDays > 0 && `${remainingDays}D:`}
-                            {remainingHours > 0 && `${remainingHours}H:`}
-                            {remainingMinutes > 0 && `${remainingMinutes}M:`}
-                            {remainingSeconds}S:
-                        </p>
+            <div className="todo-columns">
+                <div className="todo-entry">
+                {isLoggedIn && (
+                        <div className="information-container">
+                            <div className="information-top">
+                                {isUrgent && <Icon path={mdiRunFast} size={1} color={'red'} />}
+                                <Icon path={priorityIcons[priority]} size={1} />
+                            </div>
+                            <div className="information-time">
+                                <Icon path={mdiClockOutline} size={1} /> {estimatedTime ? `${estimatedTime}h` : '-'}
+                            </div>
+                            <div className="information-dif">
+                                {difficultyIcons[difficulty].map((icon, index) => (
+                                    <Icon key={index} path={icon} size={0.3} />
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                    <div className="done-item">
+                        <div className="time">
+                            <p className="time-stamp"> <strong>Completed:</strong>
+                                {completed.toLocaleDateString()} - {completed.toLocaleTimeString()}
+                            </p>
+                            <div className="separator"> </div>
+                            <p className="time-stamp">
+                                <strong> Duration: </strong>
+                                {remainingDays > 0 && `${remainingDays}D:`}
+                                {remainingHours > 0 && `${remainingHours}H:`}
+                                {remainingMinutes > 0 && `${remainingMinutes}M:`}
+                                {remainingSeconds}S:
+                            </p>
+                        </div>
+                        <div className="todo-text">
+                            <p className="done-text"> {task} </p>
+                        </div>
                     </div>
-                    <div className="checkbox-and-task">
-                        <input className="checkbox-c" type="checkbox" defaultChecked={isDone} />
-                        <p className="done-text"> {task} </p>
-                    </div>
-                </div>
-                <div className="buttons">
-                    <button className="deleteButton entryButton" onClick={handleDelete} style={{ marginTop: '26px' }}>
-                        <i className="material-icons todo-entry-icon">delete</i>
-                    </button>
+                    <div className="buttons">
+                        <button className="deleteButton entryButton" onClick={handleDelete} style={{ marginTop: '26px' }}>
+                            <i className="material-icons todo-entry-icon">delete</i>
+                        </button>
 
-                    <button className="entryButton" onClick={handleMoreInfromationClick}>
-                        <i className="material-icons todo-entry-icon"> {isMoreChecked ? "keyboard_arrow_up" : "keyboard_arrow_down"} </i>
-                    </button>
+                        <button className="entryButton" onClick={handleMoreInfromationClick}>
+                            <i className="material-icons todo-entry-icon"> {isMoreChecked ? "keyboard_arrow_up" : "keyboard_arrow_down"} </i>
+                        </button>
+                    </div>
                 </div>
+                {isMoreChecked && isLoggedIn && (
+                    <div className={`more-information ${isMoreChecked ? 'open' : ''}`}>
+                        <div className="steps-container">
+                            {steps && steps.length > 0 ? (
+                                <>
+                                    {steps.map((step, index) => (
+                                        <div key={index} className="step-entry" onClick={() => handleStepClick(step.id)}>
+                                            <p className="step-id"><strong>{`Step ${index + 1}`}</strong></p>
+                                            <p className="step-name">{step.taskName}</p>
+                                        </div>
+                                    ))}
+                                    <p className="add-step" style={{ margin: '12px' }}><strong>Add another step</strong></p>
+                                </>
+                            ) : (
+                                <div className="add-step"><strong>Add step</strong></div>
+                            )}
+                        </div>
+
+                        <div className="description-container">
+                            <p className="description-label"> <strong>Description </strong> </p>
+                            <p> {description} </p>
+                        </div>
+                    </div>
+                )}
+
+                {isMoreChecked && !isLoggedIn && (
+                    <div className={`more-information-loggedOut ${isMoreChecked ? 'open' : ''}`}>
+                        <div className="more-information-loggedOut-message">
+                            <p> <strong>Log in to access this feature </strong> </p>
+                        </div>
+                    </div>
+                )}
             </div>
         )
 
     } else if (type === 'doing' && isStarted && !isDone) {
         return (
-            <div className="todo-entry">
-                <div className="doing-item" onClick={handleClicktoComplete}>
-                    <div className="time">
-                        <p className="time-stamp"> <strong>Started:</strong> {started.toLocaleDateString()} - {started.toLocaleTimeString()} </p>
-                        {dueDate ? (
-                            <>
-                                <div className="separator"> </div>
-                                <p className="time-stamp"> <strong>Deadline:</strong> {dueDate.toLocaleDateString()} - {dueDate.toLocaleTimeString()} </p>
-                            </>
-                        ) : null}
-                        <div className="separator"> </div>
-                        <p className="time-stamp"> <strong>Time spent:</strong> {formatTime(Math.floor((currentTime - started) / 1000))} </p>
+            <div className="todo-columns">
+                <div className="todo-entry">
+                {isLoggedIn && (
+                        <div className="information-container">
+                            <div className="information-top">
+                                {isUrgent && <Icon path={mdiRunFast} size={1} color={'red'} />}
+                                <Icon path={priorityIcons[priority]} size={1} />
+                            </div>
+                            <div className="information-time">
+                                <Icon path={mdiClockOutline} size={1} /> {estimatedTime ? `${estimatedTime}h` : '-'}
+                            </div>
+                            <div className="information-dif">
+                                {difficultyIcons[difficulty].map((icon, index) => (
+                                    <Icon key={index} path={icon} size={0.3} />
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                    <div className="doing-item" onClick={handleClicktoComplete}>
+                        <div className="time">
+                            <p className="time-stamp"> <strong>Started:</strong> {started.toLocaleDateString()} - {started.toLocaleTimeString()} </p>
+                            {dueDate ? (
+                                <>
+                                    <div className="separator"> </div>
+                                    <p className="time-stamp"> <strong>Deadline:</strong> {dueDate.toLocaleDateString()} - {dueDate.toLocaleTimeString()} </p>
+                                </>
+                            ) : null}
+                            <div className="separator"> </div>
+                            <p className="time-stamp"> <strong>Time spent:</strong> {formatTime(Math.floor((currentTime - started) / 1000))} </p>
+                        </div>
+                        <div className="todo-text">
+                            <p className="doing-text" onAnimationEnd={handleAnimationEnd}> {task} </p>
+                        </div>
                     </div>
-                    <div className="checkbox-and-task">
-                        <input className="checkbox-c doing-checkbox" type="checkbox" checked={isChecked} readOnly />
-                        <p className="doing-text" onAnimationEnd={handleAnimationEnd}> {task} </p>
+                    <div className="buttons">
+                        <button className="deleteButton entryButton" onClick={handleDelete} >
+                            <i className="material-icons todo-entry-icon">delete</i>
+                        </button>
+                        <button className="entryButton" onClick={handleCancel} title="cancel task">
+                            <i className="material-icons todo-entry-icon">close_small</i>
+                        </button>
+                        <button className="entryButton" onClick={handleMoreInfromationClick}>
+                            <i className="material-icons todo-entry-icon"> {isMoreChecked ? "keyboard_arrow_up" : "keyboard_arrow_down"} </i>
+                        </button>
                     </div>
                 </div>
-                <div className="buttons">
-                    <button className="deleteButton entryButton" onClick={handleDelete} >
-                        <i className="material-icons todo-entry-icon">delete</i>
-                    </button>
-                    <button className="entryButton" onClick={handleCancel} title="cancel task">
-                        <i className="material-icons todo-entry-icon">close_small</i>
-                    </button>
-                    <button className="entryButton" onClick={handleMoreInfromationClick}>
-                        <i className="material-icons todo-entry-icon"> {isMoreChecked ? "keyboard_arrow_up" : "keyboard_arrow_down"} </i>
-                    </button>
+                {isMoreChecked && isLoggedIn && (
+                    <div className={`more-information ${isMoreChecked ? 'open' : ''}`}>
+                        <div className="steps-container">
+                            {steps && steps.length > 0 ? (
+                                <>
+                                    {steps.map((step, index) => (
+                                        <div key={index} className="step-entry" onClick={() => handleStepClick(step.id)}>
+                                            <p className="step-id"><strong>{`Step ${index + 1}`}</strong></p>
+                                            <p className="step-name">{step.taskName}</p>
+                                        </div>
+                                    ))}
+                                    <p className="add-step" style={{ margin: '12px' }}><strong>Add another step</strong></p>
+                                </>
+                            ) : (
+                                <div className="add-step"><strong>Add step</strong></div>
+                            )}
+                        </div>
 
+                        <div className="description-container">
+                            <p className="description-label"> <strong>Description </strong> </p>
+                            <p> {description} </p>
+                        </div>
+                    </div>
+                )}
+
+                {isMoreChecked && !isLoggedIn && (
+                    <div className={`more-information-loggedOut ${isMoreChecked ? 'open' : ''}`}>
+                        <div className="more-information-loggedOut-message">
+                            <p> <strong>Log in to access this feature </strong> </p>
+                        </div>
+                    </div>
+                )}
                 </div>
-            </div>
-        );
+                );
     } else {
         return null;
     }
@@ -319,33 +418,33 @@ const TodoEntry = ({ type, todoData, onEdit }) => {
 }
 
 
-TodoEntry.propTypes = {
-    todoData: PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        id: PropTypes.number.isRequired,
-        task: PropTypes.string.isRequired,
-        isDone: PropTypes.bool.isRequired,
-        created: PropTypes.instanceOf(Date).isRequired,
-        completed: PropTypes.instanceOf(Date),
-        started: PropTypes.instanceOf(Date),
-        isStarted: PropTypes.bool,
-        updatedAt: PropTypes.string,
-        description: PropTypes.string,
-        dueDate: PropTypes.instanceOf(Date),
-        inList: PropTypes.arrayOf(PropTypes.string),
-        owner: PropTypes.string,
-        priority: PropTypes.string,
-        isUrgent: PropTypes.bool,
-        difficulty: PropTypes.string,
-        estimatedTime: PropTypes.number,
-        steps: PropTypes.arrayOf(PropTypes.shape({
-            _id: PropTypes.string.isRequired,
-            taskName: PropTypes.string.isRequired,
-            isDone: PropTypes.bool.isRequired,
-            id: PropTypes.number.isRequired,
+                TodoEntry.propTypes = {
+                    todoData: PropTypes.shape({
+                    _id: PropTypes.string.isRequired,
+                id: PropTypes.number.isRequired,
+                task: PropTypes.string.isRequired,
+                isDone: PropTypes.bool.isRequired,
+                created: PropTypes.instanceOf(Date).isRequired,
+                completed: PropTypes.instanceOf(Date),
+                started: PropTypes.instanceOf(Date),
+                isStarted: PropTypes.bool,
+                updatedAt: PropTypes.string,
+                description: PropTypes.string,
+                dueDate: PropTypes.instanceOf(Date),
+                inList: PropTypes.arrayOf(PropTypes.string),
+                owner: PropTypes.string,
+                priority: PropTypes.string,
+                isUrgent: PropTypes.bool,
+                difficulty: PropTypes.string,
+                estimatedTime: PropTypes.number,
+                steps: PropTypes.arrayOf(PropTypes.shape({
+                    _id: PropTypes.string.isRequired,
+                taskName: PropTypes.string.isRequired,
+                isDone: PropTypes.bool.isRequired,
+                id: PropTypes.number.isRequired,
         })),
     }).isRequired,
-    type: PropTypes.string.isRequired,
+                type: PropTypes.string.isRequired,
 };
 
-export default TodoEntry;
+                export default TodoEntry;
