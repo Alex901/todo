a53849cb8816120b9f3752bff38d81efc56f1bce
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-09-05 15:41:40 +0200

    Updated server DNS

--- config.vite.js
+++ config.vite.js
@@ -1,5 +1,5 @@
 console.log('config.vite.js is running');
 
-const BASE_URL = import.meta.env.VITE_REACT_APP_PRODUCTION === 'true' ? 'https://todo-backend-gkdo.onrender.com' : 'http://localhost:5000';
+const BASE_URL = import.meta.env.VITE_REACT_APP_PRODUCTION === 'true' ? 'https://api.habitforge.se' : 'http://localhost:5000';
 
 export default BASE_URL;
