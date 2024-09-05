eaeb0b9fc98871972874f6288365f46410f0752b
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-09-05 15:16:44 +0200

    Changed base url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 console.log('config.js is running');
 
-const BASE_URL = process.env.VITE_REACT_APP_PRODUCTION === 'true' ? 'https://todo-backend-gkdo.onrender.com' : 'http://localhost:5000';
+const BASE_URL = process.env.VITE_REACT_APP_PRODUCTION === 'true' ? 'https://api.habitforge.se' : 'http://localhost:5000';
 console.log('DEBUG: BASE_URL in config for Jest:', BASE_URL);
 
 export default BASE_URL;
