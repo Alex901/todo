c3d5d8990f162f98caa39763c33ef5a54e70e09e
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-09-09 12:35:52 +0200

    made the bottom drawer button a little nicer

--- src/components/Mobile/BottomDrawerButton/BottomDrawerButton.jsx
+++ src/components/Mobile/BottomDrawerButton/BottomDrawerButton.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import './BottomDrawerButton.css';
 import TodoModal from '../../Todo/TodoModal/TodoModal';
-import BottomDrawer from '../BottomDrawer/BottomDrawer';
 import Icon from '@mdi/react';
-import { mdiChevronUp } from '@mdi/js';
+import { mdiChevronUp, mdiPlus } from '@mdi/js';
+import TodoButton from '../../Todo/TodoButton/TodoButton';
 
 const BottomDrawerButton = ({ listName, onOpen, onClose }) => {
     const [isTodoModalOpen, setIsTodoModalOpen] = useState(false);
@@ -16,29 +16,37 @@ const BottomDrawerButton = ({ listName, onOpen, onClose }) => {
 
     const handleOpenDrawer = () => {
         setDrawerOpen(true);
-      };
-    
-      const handleCloseDrawer = () => {
-        setDrawerOpen(false);
-      };
-    
+    };
 
+    const handleCloseDrawer = () => {
+        setDrawerOpen(false);
+    };
+
+    <TodoModal
+        isOpen={isTodoModalOpen}
+        onRequestClose={() => setIsTodoModalOpen(false)} />
 
     return (
         <div className="bottom-drawer-button-container">
-       <Button className="drawer-button left" onClick={onOpen}>
-          {listName}
-          <Icon path={mdiChevronUp} size={1} color="black" />
-        </Button>
-            <Button className="drawer-button right" onClick={handleOpenTodoModal}>
-                Create Entry
-            </Button>
-            <BottomDrawer isOpen={isDrawerOpen} onClose={handleCloseDrawer} />
+            <div className="drawer-button-section">
+                <Button className="drawer-button" onClick={onOpen} >
+                <Icon path={mdiChevronUp} size={1} color="white" className="left-icon" />
+         <span className="list-name-bottom-drawer-button"> {listName} </span>
+          <Icon path={mdiChevronUp} size={1} color="white" className="right-icon" />
+                </Button>
+            </div>
+            <div className="create-entry-section">
+                <button className="todo-button-mobile" onClick={handleOpenTodoModal}>
+                    <Icon path={mdiPlus} size={1.8} />
+                </button>
+            </div>
             <TodoModal
                 isOpen={isTodoModalOpen}
                 onRequestClose={() => setIsTodoModalOpen(false)} />
         </div>
+
     );
+
 };
 
 export default BottomDrawerButton;
