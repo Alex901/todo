5f9d7866c0938a53ae594753fed2bae2b008a537
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-09-09 12:43:50 +0200

    sticky top toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1073,7 +1073,7 @@ function App() {
               {isLoggedIn && !isMobile && <hr style={{ width: '80%', margin: '1em auto' }}></hr>}
 
               {/* Second row */}
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <div style={{ display: 'flex', justifyContent: 'center', position: 'sticky', top: 0 }}>
                 <button className="navButton" onClick={switchTodoView} style={{
                   background: activeView === 'todo' ? '#eaeaef' : '#E65151',
                   color: activeView === 'todo' ? 'black' : 'white',
